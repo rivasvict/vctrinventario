78c5ff9454d71718f18eecc6bc67277c8163b7a8
Victor Rivas <vctr90w@gmail.com>
2013-11-22 06:36:32 -0800

    demostracion de git

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<title>
-		Sistema de inventario de Alfacero Hual CA - vctr
+		Sistema de inventario de Alfacero Hual CA - vctr90
 	</title>
 	<head>
 
 	</head>
 	<body>
-
+		<p> este es un parrafo</p>
 	</body>
 </html>
