cc5902fc21a1100eedf7216bbb7468f295ff909b
Victor at VM Alfacero <vctr90w@gmail.com>
2013-11-25 08:08:44 -0800

    creando funciones conceptuales para manejo de calculos de inventario

--- classes/almacenes.php
+++ classes/almacenes.php
@@ -1,22 +1,41 @@
 <?
 
 class almacenes{
-	public 		$nombre_de_almacen 	;
+	public 		$nombreAl 		;//Nombre de almacen
 	protected	$area			;//Area de trabajo de almcen
 	public		$existencia		;//existencia en el almacen
-
-	public function crear_almacen(){
-				
+					
+	public function __construct(){
+					
 	}
 
-	private function calculo_consumo_promedio_mensual($consumosMensuales){	//Objeto que trae del resultado de la
-										//base de datos
+	private function calcPM($consumosM){
+		//calcPM funcion que calcula el CONSUMO PROMEDIO MENSUAL
+
+		//$consumosM objeto resultado de la sumatoria de los ultimos seis meses del campo de consumo mensual 
+		//con respecto a la fecha actual.
+
+		$consumoPM = $consumosM / 6;
+		return $consumoPM;
+
+		//$consumoPM consumo promedio mensual
 	}
 
-	public function calcular_alcance_material_tiempo($consumo_promedio_mensual){
 
+
+	public function calAT($consumoPM, $saldo){
+		//calAT funcion que calcula la proyeccion de duracion de materia prima en el tiempo
+
+		$alcAT = $saldo / $consumoPM;
+		return $alcAT;
+		
+		//$alcAT valor del alcance proyectado en el tiempo
+	}
+
+	public function calAP(){
+		
 	}
 
 }
-	$almacen = new almacenes;
+	//$almacen = new almacenes;
 ?>
