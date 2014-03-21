518f96fdec5aaf01efd0da94dddd784367766bdc
Victor Rivas <vctr90w@gmail.com>
2014-03-21 16:48:44 -0430

    Search textbox added and made it work

--- includes/jsQuery.php
+++ includes/jsQuery.php
@@ -1,7 +1,8 @@
+<span>Buscar:</span><input type="text" ng-model="search" placeholder="Introduzca pista para busqueda">
 <table class="displayElements">
 <tr>
 <td ng-repeat="key in notSorted(ftittle)">{{ftittle[key]}}</td><tr>
-<tr ng-repeat="val in filler">
+<tr ng-repeat="val in filler | filter:search" class="search-query">
 <td ng-repeat="key in notSorted(val)" ng-show="!$last">{{val[key]}}</span>
 </td>
 <td><a href='' ng-click='editar(val)'>Editar</a> / <a href=''>Eliminar</a>
